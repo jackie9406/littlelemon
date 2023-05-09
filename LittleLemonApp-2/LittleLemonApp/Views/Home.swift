a989a221903b0b7c6462b66c1d241f6597698e95
jackie9406 <119382673+jackie9406@users.noreply.github.com>
2023-05-08 20:27:35 -0500

    Add files via upload

---
+++ LittleLemonApp-2/LittleLemonApp/Views/Home.swift
@@ -0,0 +1,22 @@
+//
+//  Home.swift
+//  LittleLemonApp
+//
+//  Created by Jacqueline Ford on 05/06/2023.
+//
+
+import SwiftUI
+
+struct Home: View {
+    
+    var body: some View {
+        MainScreen()
+            .navigationBarBackButtonHidden()
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home().environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
+    }
+}
